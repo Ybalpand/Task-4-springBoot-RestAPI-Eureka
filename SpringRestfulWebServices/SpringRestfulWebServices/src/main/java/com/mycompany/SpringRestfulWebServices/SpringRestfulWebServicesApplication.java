7ea383aaf676be20dd97ddae4fa8ae0b4c8a6ec9
Ybalpand <64777781+Ybalpand@users.noreply.github.com>
2020-06-30 17:48:33 +0530

    upload 3 project 2 client and 1 eureka server

---
+++ SpringRestfulWebServices/SpringRestfulWebServices/src/main/java/com/mycompany/SpringRestfulWebServices/SpringRestfulWebServicesApplication.java
@@ -0,0 +1,15 @@
+package com.mycompany.SpringRestfulWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulWebServicesApplication.class, args);
+	}
+
+}
